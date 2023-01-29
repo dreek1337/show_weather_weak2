09dfe4b49ebef3d592d804a997f97874e3905534
dreek1337 <danil.sapog19@gmail.com>
2023-01-29 11:13:10 +0300

    Декоратор

---
+++ decorators/decorators.py
@@ -0,0 +1,46 @@
+import time
+
+
+def message(func):
+    def inner(*args, **kwargs):
+        print("---Start function---")
+
+        a = func(*args, **kwargs)
+
+        print("---End function---")
+
+        return a
+
+    return inner
+
+
+def time_it(func):
+    def inner(*args, **kwargs):
+        now = time.time()
+        a = func(*args, **kwargs)
+        print(f'Function time completed: {round(time.time() - now, 2)}')
+        return a
+    return inner
+
+
+def catch_exception(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as err:
+            print(f'Error: {err}')
+    return inner
+
+
+@message
+@catch_exception
+@time_it
+def test_def(a, b):
+    list(range(10000000)) * 2
+    res = a / b
+    print(res)
+    return res
+
+
+test_def(2, 2)
+test_def(2, 0)
